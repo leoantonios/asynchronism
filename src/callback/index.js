e4dcabb9d1045ca10e16ece8164b2d3581674cb3
Leonardo <leobuchini@gmail.com>
2021-03-14 18:25:26 -0500

    Second Callback

--- src/callback/index.js
+++ src/callback/index.js
@@ -6,4 +6,18 @@ function calc(number1, number2, callback) {
     return callback(number1, number2);
 }
 
-console.log(calc(2,3,add))
+console.log(calc(2,3,add));
+
+function date(callback) {
+    console.log(new Date);
+    setTimeout(function () {
+        let date = new Date;
+        callback(date);
+    },3000)
+}
+
+function printDate(dateNow) {
+    console.log(dateNow)
+}
+
+date(printDate);
