f254d541cfa96797deeb7d1bbf14c56b8d9bee0f
Leonardo <leobuchini@gmail.com>
2021-03-14 21:21:51 -0500

    Promise Finish

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,19 +1,25 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-let api = 'https://rickandmortyapi.com/api/character/';
 
-function fetchData(url_api, callback){
-    let xhttp = new XMLHttpRequest();
-    xhttp.open('GET',url_api, true);
-    xhttp.onreadystatechange = function( event ){
-        if (xhttp.readyState === 4) {
-            if (xhttp.status === 200) {
-                callback(null, JSON.parse(xhttp.responseText))
-            }else{
-                const error = new Error('Error '+ url_api);
-                return callback(error, null)
+const fetchData = (url_api) =>{
+
+    return new Promise((resolve,reject)=>{
+        const xhttp = new XMLHttpRequest();
+        xhttp.open('GET',url_api, true);
+        xhttp.onreadystatechange = (() => {
+            if (xhttp.readyState === 4) {
+                
+                (xhttp.status === 200)
+                    ? resolve(JSON.parse(xhttp.responseText))
+                    : reject (new Error('Error '+ url_api));
+
             }
-        }
-    }
-    xhttp.send();
+        });
+        xhttp.send();
+    
+    
+    });
+    
 }
+
+module.exports = fetchData;
