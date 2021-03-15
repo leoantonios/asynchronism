57b650b0985fb18284526e7275f1650ac5ad3514
Leonardo <leobuchini@gmail.com>
2021-03-14 20:55:13 -0500

    Add Utils, and challenge promise

--- src/promise/index.js
+++ src/promise/index.js
@@ -11,3 +11,28 @@ const somethingWillHappen = () => {
 somethingWillHappen()
 .then(response => console.log(response))
 .catch(err => console.error(err))
+
+const somethingWillHappen2 = () =>{
+    return new Promise((resolve,reject)=>{
+        if (true) {
+            setTimeout(() => {
+                resolve('True')
+            }, 2000);
+        }else{
+            const error = new Error('Oh no!');
+            reject(error);
+        }
+    })
+}
+
+somethingWillHappen2()
+.then(response => console.log(response))
+.catch(err => console.error(err))
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+.then(response =>{
+    console.log('Array of results: ', response);
+})
+.catch(err=>{
+    console.error(err);
+})
