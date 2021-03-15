6e6b1a6b252486e41291777d125831800dc83f41
Leonardo <leobuchini@gmail.com>
2021-03-14 20:30:01 -0500

    Finish test Callback

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,6 +1,8 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-function fetchData(url, callback){
+let api = 'https://rickandmortyapi.com/api/character/';
+
+function fetchData(url_api, callback){
     let xhttp = new XMLHttpRequest();
     xhttp.open('GET',url_api, true);
     xhttp.onreadystatechange = function( event ){
@@ -15,3 +17,16 @@ function fetchData(url, callback){
     }
     xhttp.send();
 }
+
+fetchData(api, function(err, response1){
+    if (err) return console.error(err);
+    fetchData(api + response1.results[0].id, function(err2, response2){
+        if (err2) return console.error(err2);
+        fetchData(response2.origin.url, function(err3, response3){
+            if (err3) return console.error(err3);
+            console.log(response1.info.count);
+            console.log(response2.name);
+            console.log(response3.dimension);
+        });
+    })
+})
